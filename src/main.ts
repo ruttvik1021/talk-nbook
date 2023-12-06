e8212fb769e8371021f3a5dab755d5a7b20061b9
Ruttvik21 <rkhollam21@live.com>
2023-12-07 01:23:29 +0530

    cors enabled

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
